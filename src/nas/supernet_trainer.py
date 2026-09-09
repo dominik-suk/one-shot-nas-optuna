1e7cf4a3bdb1cec7921b273c3711dec1e5c099cf
dominik <dominik.sukic@wwb.de>
2026-09-10 00:00:46 +0200

    Replaced all training logic with train_model.train() and added SupernetTrainingWrapper, so the supernet can be trained like a normal torch module. Forward pass is always a new random path/architecture

--- src/nas/supernet_trainer.py
+++ src/nas/supernet_trainer.py
@@ -1,106 +1,75 @@
-import glob
 import os
+from collections import OrderedDict
+from typing import Any
 
 import optuna
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from optuna.samplers import RandomSampler
-from torch.utils.data import DataLoader
 
 from external.sample_blocks import Sampler
+from src.data.pamap2_labels import Pamap2ActivityType
 from src.models.supernet import Supernet
-from src.utils.supernet_io import load_supernet, save_supernet
+from src.models.train_model import train
+from src.logging.train_logger import TrainLogger
+
+
+class SupernetTrainingWrapper(nn.Module):
+    def __init__(self, supernet: Supernet, search_space: dict, fixed_architecture_config: OrderedDict[Any, Any] = None, ):
+        super().__init__()
+        self.supernet = supernet
+        self.search_space = search_space
+        self.fixed_architecture = self._init_fixed_architecture(fixed_architecture_config)
+
+    def _take_one_sample(self) -> OrderedDict[Any, Any]:
+        return self._get_new_sampler().construct_sample(self.search_space)
+
+    def _init_fixed_architecture(self, fixed_architecture_config):
+        if fixed_architecture_config is not None:
+            return self._get_new_sampler().construct_sample(fixed_architecture_config)
+        return self._take_one_sample()
+
+    @staticmethod
+    def _get_new_sampler():
+        study = optuna.create_study(sampler=RandomSampler())
+        trial = study.ask()
+        sampler = Sampler(trial)
+        return sampler
+
+    def forward(self, x):
+        if self.training:
+            architecture = self._take_one_sample()
+        else:
+            architecture = self.fixed_architecture
+
+        return self.supernet(x, architecture)
 
 
 def train_supernet(
         supernet: Supernet,
-        train_loader: DataLoader,
-        validation_loader: DataLoader,
         search_space: dict,
-        validation_search_space: dict,
         epochs: int = 100,
         device: str = "cuda",
-        save_dir: str = "models/supernet",
+        activity_type: Pamap2ActivityType = Pamap2ActivityType.ADL,
+        fixed_architecture_config = None,
+        save_dir: str = None,
 ):
-    supernet.to(device)
-    optimizer = optim.Adam(supernet.parameters(), lr=1e-3, weight_decay=1e-4)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs)
-    criterion = nn.CrossEntropyLoss()
+    wrapped_supernet = SupernetTrainingWrapper(
+        supernet,
+        search_space,
+        fixed_architecture_config=fixed_architecture_config
+    )
 
-    start_epoch = 0
-    best_validation_loss = float('inf')
-    checkpoint_dir = os.path.join(save_dir, 'checkpoints')
-    existing_checkpoints = glob.glob(os.path.join(checkpoint_dir, 'supernet_epoch_*.pth'))
+    train(
+        model=wrapped_supernet,
+        max_epochs=epochs,
+        device=device,
+        activity_type=activity_type,
+        logger=TrainLogger(),
+        sequence_length=search_space["input"][1]
+    )
 
-    if existing_checkpoints:
-        latest_checkpoint = max(existing_checkpoints, key=lambda x: int(x.split('_')[-1].split('.')[0]))
-        best_validation_loss = torch.load(latest_checkpoint, map_location=device, weights_only=False)['validation_loss']
-        start_epoch = load_supernet(supernet, latest_checkpoint, optimizer, device) + 1
-        for _ in range(start_epoch):
-            scheduler.step()
-
-    for epoch in range(start_epoch, epochs):
-        supernet.train()
-        total_train_loss = 0.0
-        train_study = optuna.create_study(sampler=RandomSampler())
-        train_correct = 0
-        train_total = 0
-
-        for batch_index, (batch_x, batch_y) in enumerate(train_loader):
-            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-            optimizer.zero_grad()
-            trial = train_study.ask()
-            sampler = Sampler(trial)
-            architecture_config = sampler.construct_sample(search_space)
-            output = supernet(batch_x, architecture_config)
-            loss = criterion(output, batch_y)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(supernet.parameters(), max_norm=1)
-            optimizer.step()
-            total_train_loss += loss.item()
-            predictions = torch.argmax(output, dim=1)
-            train_correct += torch.sum(predictions == batch_y).sum().item()
-            train_total += batch_y.size(0)
-
-        # avg_train_loss = total_train_loss / len(train_loader)
-        train_accuracy = train_correct / train_total
-        # current_learning_rate = scheduler.get_last_lr()[0]
-        scheduler.step()
-
-        supernet.eval()
-        total_validation_loss = 0.0
-        validation_correct = 0
-        validation_total = 0
-        validation_study = optuna.create_study(sampler=RandomSampler())
-        validation_trial = validation_study.ask()
-        validation_sampler = Sampler(validation_trial)
-        # fixed_architecture_config = validation_sampler.construct_sample(validation_search_space)
-
-        with torch.no_grad():
-            for batch_x, batch_y in validation_loader:
-                batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-                architecture = validation_sampler.construct_sample(search_space)
-                output = supernet(batch_x, architecture)
-                # output = supernet(batch_x, fixed_architecture_config)
-                total_validation_loss += criterion(output, batch_y).item()
-                predictions = torch.argmax(output, dim=1)
-                validation_correct += (predictions == batch_y).sum().item()
-                validation_total += batch_y.size(0)
-
-        avg_validation_loss = total_validation_loss / len(validation_loader)
-        validation_accuracy = validation_correct / validation_total
-        print(f'Epoch: {epoch+1}/{epochs}, Train Accuracy: {train_accuracy*100:.2f}%, Validation Accuracy: {validation_accuracy*100:.2f}%')
-        is_best = avg_validation_loss < best_validation_loss
-        if is_best:
-            best_validation_loss = avg_validation_loss
-        if is_best or epoch % 10 == 0:
-            save_supernet(
-                supernet,
-                epoch,
-                optimizer,
-                validation_loss=avg_validation_loss,
-                is_best=is_best,
-                save_dir=save_dir
-            )
-    return supernet
+    if save_dir is not None:
+        os.makedirs(save_dir, exist_ok=True)
+        save_path = os.path.join(save_dir, 'Supernet.pt')
+        torch.save(supernet, save_path)
