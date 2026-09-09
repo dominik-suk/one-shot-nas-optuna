11591de63262cb87866ab2d7508e7d429e0d1d78
dominik <dominik.sukic@wwb.de>
2026-09-09 23:56:06 +0200

    renamed train_supernet.py to supernet_do_complete_train_run.py

---
+++ scripts/supernet_do_complete_train_run.py
@@ -0,0 +1,28 @@
+import optuna
+
+from src.data.pamap2_loader import Pamap2ActivityType
+from src.models.supernet import Supernet
+from src.nas.supernet_trainer import train_supernet
+from src.paths import SUPERNET_DIR
+from src.utils.yaml_io import load_pamap2_search_space, load_pamap2_fixed_arch_config
+
+
+def main():
+    optuna.logging.set_verbosity(optuna.logging.WARNING)
+    search_space = load_pamap2_search_space()
+    fixed_architecture_config = load_pamap2_fixed_arch_config()
+
+    supernet = Supernet(search_space)
+
+    train_supernet(
+        supernet=supernet,
+        search_space=search_space,
+        fixed_architecture_config=fixed_architecture_config,
+        epochs=100,
+        activity_type=Pamap2ActivityType.ADL,
+        save_dir=SUPERNET_DIR
+    )
+
+
+if __name__ == '__main__':
+    main()
