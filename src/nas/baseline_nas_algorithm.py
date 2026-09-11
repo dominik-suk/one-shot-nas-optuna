8d9c856834b768c6962a0ea66d006df41b05aa12
dominik <dominik.sukic@wwb.de>
2026-09-12 00:35:32 +0200

    Objective function now returns accuracy Refactor: rename some variables to be clearer

--- src/nas/baseline_nas_algorithm.py
+++ src/nas/baseline_nas_algorithm.py
@@ -13,24 +13,26 @@ class BaselineNASExperiment(NASExperiment):
             study: optuna.Study,
             search_space: dict,
             activity_type: Pamap2ActivityType = Pamap2ActivityType.PROTOCOL,
-            epochs: int = 50,
-            proxy: int = 10
+            max_epochs: int = 50,
+            n_proxy_epochs: int = 10
     ):
-        super().__init__(study, search_space, activity_type, epochs)
-        self.proxy = proxy
+        super().__init__(study, search_space, activity_type, max_epochs)
+        self.n_proxy_epochs = n_proxy_epochs
 
     def objective(self, trial: optuna.Trial) -> float:
         architecture = self.sample_architecture(trial)
         model = self.create_model(architecture)
         summary: ModelSummary = train(
-            model,
-            trial=trial,
+            model=model,
             max_epochs=self.epochs,
+            n_proxy_epochs=self.n_proxy_epochs,
             activity_type=self.activity_type,
-            logger=TrainLogger(),
             sequence_length=self.input_shape[1],
+            load_best_weights=True,
+            retraining_best_model=False,
+            logger=TrainLogger(),
         )
-        return summary.loss
+        return summary.accuracy
 
     def get_best_architecture(self):
         return self.sample_architecture(self.study.best_trial)
