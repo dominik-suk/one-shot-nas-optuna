e988d26c8d399411374304b5c9ad192c83a9f1f5
dominik <dominik.sukic@wwb.de>
2026-09-12 00:33:59 +0200

    Remove pruner adjust evolutionary algorithm population size direction is now maximize because objective function score is accuracy adjust n trials

--- scripts/baseline_do_nas_run.py
+++ scripts/baseline_do_nas_run.py
@@ -1,31 +1,38 @@
 import optuna
-from optuna.pruners import HyperbandPruner
 from optuna.samplers import NSGAIISampler
 
 from src.nas.baseline_nas_algorithm import BaselineNASExperiment
-from src.paths import PAMAP2_BASELINE_EXPERIMENT_DB_PATH, BASELINE_MODEL_PATH
+from src.paths import PAMAP2_BASELINE_EXPERIMENT_DB_PATH, BASELINE_BEST_MODEL_PATH
 from src.utils.yaml_io import load_pamap2_search_space
+from src.data.pamap2_labels import Pamap2ActivityType
 
 
 def main():
     search_space = load_pamap2_search_space()
     db_url = f"sqlite:///{PAMAP2_BASELINE_EXPERIMENT_DB_PATH}"
-    optuna_sampler = NSGAIISampler(population_size=20)
+
+    optuna_sampler = NSGAIISampler(
+        population_size=10
+    )
 
     study = optuna.create_study(
         study_name='pamap2_baseline_nas',
         storage=db_url,
         load_if_exists=True,
         sampler=optuna_sampler,
-        pruner=HyperbandPruner(
-            min_resource=10,
-            max_resource=50,
-        ),
-        direction='minimize'
+        direction='maximize'
     )
-    experiment = BaselineNASExperiment(study, search_space)
-    experiment.run(200)
-    experiment.train_best_model(save_path=BASELINE_MODEL_PATH)
+
+    experiment = BaselineNASExperiment(
+        study=study,
+        search_space=search_space,
+        activity_type=Pamap2ActivityType.PROTOCOL,
+        max_epochs=50,
+        n_proxy_epochs=15,
+    )
+
+    experiment.run(n_trials=100)
+    experiment.train_best_model(save_path=BASELINE_BEST_MODEL_PATH)
 
 
 if __name__ == "__main__":
