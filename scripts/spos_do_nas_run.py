136d36c0fd9e705c34a542cc85e50ceeefbf5eb1
dominik <dominik.sukic@wwb.de>
2026-09-14 14:39:09 +0200

    Renamed and updated paths

--- scripts/spos_do_nas_run.py
+++ scripts/spos_do_nas_run.py
@@ -4,26 +4,22 @@ from optuna.samplers import NSGAIISampler
 
 from src.models.supernet import Supernet
 from src.nas.spos_nas import SinglePathOneShotNASExperiment
-from src.paths import PAMAP2_SPOS_EXPERIMENT_DB_PATH, SUPERNET_PATH, SUPERNET_BEST_MODEL_PATH
+from src.paths import SUPERNET_PATH, SPOS_EXPERIMENT_DB_PATH, SPOS_BEST_MODEL_PATH, SPOS_RANDOM_EXPERIMENT_DB_PATH, SPOS_RANDOM_BEST_MODEL_PATH
 from src.utils.yaml_io import load_pamap2_search_space
 
 
 def main():
     search_space = load_pamap2_search_space()
-    db_url = f"sqlite:///{PAMAP2_SPOS_EXPERIMENT_DB_PATH}"
+    db_url = f"sqlite:///{SPOS_EXPERIMENT_DB_PATH}"
 
     supernet = Supernet(search_space)
     supernet.load_state_dict(torch.load(SUPERNET_PATH))
-
-    optuna_sampler = NSGAIISampler(
-        population_size=50
-    )
 
     study = optuna.create_study(
         study_name='pamap2_spos_nas',
         storage=db_url,
         load_if_exists=True,
-        sampler=optuna_sampler,
+        sampler=NSGAIISampler(population_size=50),
         direction='maximize'
     )
 
@@ -35,7 +31,7 @@ def main():
     )
 
     experiment.run(1000)
-    experiment.train_best_model(save_path=SUPERNET_BEST_MODEL_PATH)
+    experiment.train_best_model(save_path=SPOS_BEST_MODEL_PATH)
 
 
 if __name__ == "__main__":
