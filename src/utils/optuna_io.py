a578db54f5a5732ea01c692c34fa0effd9e9f095
dominik <dominik.sukic@wwb.de>
2026-09-14 15:54:16 +0200

    added optuna io for getting studies

--- src/utils/optuna_io.py
+++ src/utils/optuna_io.py
@@ -13,7 +13,7 @@ def get_baseline_study() -> optuna.Study:
     return get_study(BASELINE_EXPERIMENT_DB_PATH)
 
 
-def get_spos_study(random_search: bool) -> optuna.Study:
+def get_spos_study(random_search: bool = False) -> optuna.Study:
     if random_search:
         return get_study(SPOS_RANDOM_EXPERIMENT_DB_PATH)
     return get_study(SPOS_EXPERIMENT_DB_PATH)
