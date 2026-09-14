69ef4af47bf66271a4c085e18b1364016150f63f
dominik <dominik.sukic@wwb.de>
2026-09-14 15:52:47 +0200

    Remove unused enum

--- src/utils/model_io.py
+++ src/utils/model_io.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from pathlib import Path
 
 import optuna
@@ -7,7 +6,6 @@ from torch import nn
 
 from external.build_model import construct_model
 from external.sample_blocks import Sampler
-from src.utils.yaml_io import load_pamap2_search_space
 from src.paths import (
     BASELINE_BEST_MODEL_PATH,
     BASELINE_EXPERIMENT_DB_PATH,
@@ -16,20 +14,12 @@ from src.paths import (
     SPOS_RANDOM_BEST_MODEL_PATH,
     SPOS_RANDOM_EXPERIMENT_DB_PATH,
 )
-
-
-class NAS_Algorithm(Enum):
-    SPOS = "spos"
-    BASELINE = "baseline"
-
-
-class SearchStrategy(Enum):
-    NSGAII = "nsgaii"
-    RANDOM = "random"
+from src.utils.optuna_io import get_study
+from src.utils.yaml_io import load_pamap2_search_space
 
 
 def load_model(state_dict_path: Path, db_path: Path) -> nn.Module:
-    study = optuna.load_study(storage=f"sqlite:///{db_path}")
+    study = get_study(db_path)
     model = load_best_model(study=study)
     state_dict = torch.load(f=state_dict_path)
     model.load_state_dict(state_dict)
@@ -41,10 +31,6 @@ def load_best_model(study: optuna.Study) -> nn.Module:
     best_trial = study.best_trial
     best_architecture = Sampler(best_trial).construct_sample(search_space=search_space)
     return construct_model(sample=best_architecture, in_dim=search_space["input"], out_dim=search_space["output"])
-
-
-def get_study(path: Path | str) -> optuna.Study:
-    return optuna.load_study(storage=f"sqlite:///{path}")
 
 
 def load_spos_model(random_search: bool = False) -> nn.Module:
