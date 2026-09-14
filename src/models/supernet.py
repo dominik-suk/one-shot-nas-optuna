a260a4f314fb562fa0fe4ee4e71445c1875cf25d
dominik <dominik.sukic@wwb.de>
2026-09-14 15:21:32 +0200

    Removed LSTM logic and added standalone DynamicLSTM layer in choice blocks Ensuring last layer is always classification layer with no activation function Moved flattening logic for linear layers to choice blocks _get_max_channels now returns max_hidden_size * 2 if the search space allows for lstm layers with bidirectional=True

--- src/models/supernet.py
+++ src/models/supernet.py
@@ -1,21 +1,17 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-from src.models.choice_blocks import ChoiceBlock, DynamicConv1d, DynamicLinear, GaussianDropout
+from src.models.choice_blocks import ChoiceBlock, DynamicConv1d, DynamicLSTM, DynamicLinear, GaussianDropout
 
 
 class Supernet(nn.Module):
     def __init__(self, search_space: dict):
         super().__init__()
         self.search_space: dict = search_space
+        self.in_sensors: int = self.search_space["input"][0]
         self.num_classes: int = search_space["output"]
         self.default_op_params: dict = search_space.get("default_op_params", {})
-        input_shape = search_space["input"]
-        in_sensors = input_shape[0]
-        self.max_channels = self._get_max_channels()
-
-        self.input = nn.Conv1d(in_sensors, self.max_channels, 1)
+        self.max_channels: int = self._get_max_channels()
         self.blocks = nn.ModuleDict()
 
         for block_config in search_space.get("sequence", []):
@@ -37,41 +33,41 @@ class Supernet(nn.Module):
                     match op:
                         case "identity":
                             continue
+
                         case "conv1d":
-                            kernel_sizes = self.default_op_params["conv1d"]["kernel_size"]
                             candidates["conv1d"] = DynamicConv1d(
-                                in_channels_max=self.max_channels,
-                                out_channels_max=self.max_channels,
-                                kernel_sizes=kernel_sizes
+                                max_channels=self.max_channels,
+                                kernel_sizes=self.default_op_params["conv1d"]["kernel_size"]
                             )
-                        case "maxpool":
-                            candidates["maxpool"] = nn.MaxPool1d(1, 1)
+
                         case "lstm":
-                            hidden_size_max = max(self.default_op_params["lstm"]["hidden_size"])
-                            candidates["lstm"] = nn.LSTM(
-                                input_size=self.max_channels,
-                                hidden_size=hidden_size_max,
-                                num_layers=2,
-                                batch_first=True,
-                                bidirectional=True
+                            hidden_sizes = self.default_op_params["lstm"]["hidden_size"]
+                            num_layers = self.default_op_params["lstm"]["num_layers"]
+                            candidates["lstm"] = DynamicLSTM(
+                                max_channels=self.max_channels,
+                                max_hidden_size=max(hidden_sizes),
+                                max_num_layers=max(num_layers)
                             )
+
+                        case "linear":
+                            candidates["linear"] = DynamicLinear(max_channels=self.max_channels)
+
+                        case "maxpool":
+                            candidates["maxpool"] = nn.MaxPool1d(2, 2)
+
                         case "dropout":
                             candidates["dropout"] = nn.Dropout(p=0.5)
+
                         case "gaussian_dropout":
                             candidates["gaussian_dropout"] = GaussianDropout(p=0.5)
-                        case "linear":
-                            candidates["linear"] = DynamicLinear(
-                                self.max_channels,
-                                self.max_channels
-                            )
+
                 layer_list.append(ChoiceBlock(f"{block_id}_l{layer_index}", candidates))
+
             self.blocks[block_id] = layer_list
-        self.classifier = nn.Linear(self.max_channels, self.num_classes)
 
-    def forward(self, x: torch.Tensor, model_sample: dict): # x: [Batch Size, Features, Sequence Length]
-        x = self.input(x)
-
-        for block_id, layers in self.blocks.items():
+    def forward(self, x: torch.Tensor, model_sample: dict):
+        # x: [Batch Size, Features, Sequence Length]
+        for block_index, (block_id, layers) in enumerate(self.blocks.items()):
             if block_id not in model_sample:
                 continue
 
@@ -85,49 +81,46 @@ class Supernet(nn.Module):
                 op_name = layer_sample["operation"]
                 op_params = layer_sample["params"]
 
-                match op_name:
-                    case "lstm":
-                        x = self._pad(x)
-                        x = x.transpose(1, 2)
-                        lstm_out, _ = choice_block.candidates["lstm"](x)
-                        active_channel_count = lstm_out.shape[2] # todo: Channel size doesn't have to stay the same, can double
-                        x = (lstm_out[:, :, :active_channel_count // 2] + lstm_out[:, :, active_channel_count // 2:]) / 2
-                        x = x.transpose(1, 2)
+                if self._is_last_layer_of_last_block(block_index, layer_index, block_sample):
+                    op_params["width"] = self.num_classes
+                    op_params["activation"] = None
 
-                    case "linear":
-                        x = self._flatten(x)
-                        x = self._pad(x)
-                        x = choice_block(x, op_name, op_params)
+                x = choice_block(x, op_name, op_params)
 
-                    case _:
-                        x = choice_block(x, op_name, op_params)
+        return x
 
-        x = self._flatten(x)
-        x = self._pad(x)
-
-        return self.classifier(x)
+    def _is_last_layer_of_last_block(self, block_index, layer_index, block_sample):
+        return block_index >= len(self.blocks) - 1 and f"l{layer_index + 1}" not in block_sample
 
     @staticmethod
-    def _flatten(x: torch.Tensor):
-        if x.dim() == 3:
-            x = x.mean(dim=-1)
-        return x
+    def _is_last_layer(layer_index, block_sample):
+        return f"l{layer_index + 1}" not in block_sample
 
-    def _pad(self, x: torch.Tensor):
-        current_channels = x.shape[1]
-        if current_channels < self.max_channels:
-            difference = self.max_channels - current_channels
-            if x.dim() == 3:
-                x = F.pad(x, (0, 0, 0, difference))
-            else:
-                x = F.pad(x, (0, difference))
-        return x
-
-    def _get_max_channels(self): # todo: max hidden size must be x2 if bidirectional=True
-        max_channels = 0
+    def _get_max_channels(self):
         channel_size_keys = {"out_channels", "hidden_size", "width"}
+        max_channels = 0
+
         for op_name, params in self.default_op_params.items():
             for key, values in params.items():
                 if key in channel_size_keys:
-                    max_channels = max(max_channels, max(values))
-        return max_channels
+                    if isinstance(values, int):
+                        max_channels = max(max_channels, values)
+                    else:
+                        max_channels = max(max_channels, max(values))
+
+        if self._can_be_bidirectional():
+            max_channels = max(max_channels, self._get_max_hidden_size() * 2)
+
+        return max(max_channels, self.in_sensors)
+
+    def _can_be_bidirectional(self) -> bool:
+        possible_bidirectional_values: list[bool] | bool = self.default_op_params.get("lstm", {}).get("bidirectional", False)
+        if isinstance(possible_bidirectional_values, list):
+            return any(possible_bidirectional_values)
+        return possible_bidirectional_values
+
+    def _get_max_hidden_size(self) -> int:
+        hidden_sizes = self.default_op_params.get("lstm", {}).get("hidden_size", 0)
+        if isinstance(hidden_sizes, list):
+            return max(hidden_sizes)
+        return hidden_sizes
