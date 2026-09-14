0f6605221f4bcb7795099faabd2382fabaebbcac
dominik <dominik.sukic@wwb.de>
2026-09-14 14:47:08 +0200

    Added DynamicLSTM module. Since lstm weights can't be sliced easily in pytorch, instantiated lstm layer with maximum size and padded difference of active_hidden_size with zeroes Added possible activation functions to DynamicLinear and DynamicConv1d modules

--- src/models/choice_blocks.py
+++ src/models/choice_blocks.py
@@ -19,11 +19,19 @@ class ChoiceBlock(nn.Module):
             out_channels = params["out_channels"]
             kernel_size = params["kernel_size"]
             stride = params["stride"]
-            return module(x, active_out_channels=out_channels, active_kernel_size=kernel_size, stride=stride)
+            activation = params.get("activation", "relu")
+            return module(x, active_out_channels=out_channels, active_kernel_size=kernel_size, activation=activation, stride=stride)
+
+        elif isinstance(module, DynamicLSTM):
+            hidden_size = params["hidden_size"]
+            num_layers = params["num_layers"]
+            bidirectional = params["bidirectional"]
+            return module(x, active_hidden_size=hidden_size, num_layers=num_layers, bidirectional=bidirectional)
 
         elif isinstance(module, DynamicLinear):
             width = params["width"]
-            return module(x, active_out_features=width)
+            activation = params.get("activation", None)
+            return module(x, active_out_features=width, activation=activation)
 
         elif isinstance(module, nn.MaxPool1d):
             kernel_size = params["kernel_size"]
@@ -39,44 +47,109 @@ class ChoiceBlock(nn.Module):
 
 
 class DynamicConv1d(nn.Module):
-    def __init__(self, in_channels_max, out_channels_max, kernel_sizes: list[int]):
+    def __init__(self, max_channels: int, kernel_sizes: list[int]):
         super().__init__()
-        self.in_channels_max = in_channels_max
-        self.out_channels_max = out_channels_max
         self.conv_dict = nn.ModuleDict()
 
         for kernel_size in kernel_sizes:
             self.conv_dict[str(kernel_size)] = nn.Conv1d(
-                in_channels=in_channels_max,
-                out_channels=out_channels_max,
+                in_channels=max_channels,
+                out_channels=max_channels,
                 kernel_size=kernel_size,
                 padding=kernel_size // 2
             )
 
-    def forward(self, x: torch.Tensor, active_out_channels: int, active_kernel_size: int, stride: int = 1):
+    def forward(self, x: torch.Tensor, active_out_channels: int, active_kernel_size: int,  activation: str | None, stride: int = 1):
         active_in_channels = x.shape[1]
         conv = self.conv_dict[str(active_kernel_size)]
         weight = conv.weight[:active_out_channels, :active_in_channels, :]
         bias = conv.bias[:active_out_channels] if conv.bias is not None else None
         padding = active_kernel_size // 2
 
-        return F.conv1d(x, weight, bias=bias, stride=stride, padding=padding)
+        x = F.conv1d(x, weight, bias=bias, stride=stride, padding=padding)
+        if activation == "relu":
+            return F.relu(x)
+        elif activation == "tanh":
+            return F.tanh(x)
+        return x
+
+
+class DynamicLSTM(nn.Module):
+    def __init__(self, max_channels: int, max_hidden_size: int, max_num_layers: int):
+        super().__init__()
+        self.max_channels = max_channels
+        self.max_hidden_size = max_hidden_size
+        self.lstm_layers = nn.ModuleList([
+            nn.LSTM(
+                input_size=max_channels if self._is_first_layer(i) else max_hidden_size * 2,
+                hidden_size=max_hidden_size,
+                num_layers=1,
+                bidirectional=True,
+                batch_first=True
+            ) for i in range(max_num_layers)
+        ])
+
+    def forward(self, x: torch.Tensor, active_hidden_size: int, num_layers: int, bidirectional: bool):
+        # x: [Batch, Channels, Length]
+        input_size = x.shape[1]
+
+        if input_size < self.max_channels:
+            difference = self.max_channels - input_size
+            x = F.pad(x, (0, 0, 0, difference))
+
+        # x: [Batch, Length, Channels]
+        x = x.transpose(1, 2)
+
+        for i in range(num_layers):
+            x, _ = self.lstm_layers[i](x)
+
+            if not self._is_last_layer(i, num_layers) and not bidirectional:
+                x = self._fill_backward_direction_with_zeros(x)
+
+        forward = x[:, :, :active_hidden_size]
+        backward = x[:, :, self.max_hidden_size : self.max_hidden_size + active_hidden_size]
+
+        if bidirectional:
+            x = torch.cat([forward, backward], dim=2)
+        else:
+            x = forward
+
+        # x: [Batch, Channels, Length]
+        return x.transpose(1, 2)
+
+    @staticmethod
+    def _is_last_layer(i, num_layers) -> bool:
+        return i >= num_layers - 1
+
+    @staticmethod
+    def _is_first_layer(i) -> bool:
+        return i == 0
+
+    def _fill_backward_direction_with_zeros(self, x):
+        forward = x[:, :, :self.max_hidden_size]
+        return F.pad(forward, (0, self.max_hidden_size))
 
 
 class DynamicLinear(nn.Module):
-    def __init__(self, in_features_max, out_features_max):
+    def __init__(self, max_channels: int):
         super().__init__()
         self.linear = nn.Linear(
-            in_features=in_features_max,
-            out_features=out_features_max,
+            in_features=max_channels,
+            out_features=max_channels,
         )
 
-    def forward(self, x: torch.Tensor, active_out_features: int):
+    def forward(self, x: torch.Tensor, active_out_features: int, activation: str | None):
         active_in_features = x.shape[1]
         weight = self.linear.weight[:active_out_features, :active_in_features]
         bias = self.linear.bias[:active_out_features] if self.linear.bias is not None else None
+        x = F.linear(x, weight, bias=bias)
 
-        return F.linear(x, weight, bias=bias)
+        if activation == "relu":
+            return F.relu(x)
+        elif activation == "tanh":
+            return F.tanh(x)
+        else:
+            return x
 
 
 class GaussianDropout(nn.Module):
