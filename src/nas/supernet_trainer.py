a542851c26b88a57d6eca99b5a2e1bec1148468c
dominik <dominik.sukic@wwb.de>
2026-09-18 12:28:19 +0200

    Supernet can now be evaluated using n random architectures + fixed search space

--- src/nas/supernet_trainer.py
+++ src/nas/supernet_trainer.py
@@ -10,35 +10,33 @@ from optuna.samplers import RandomSampler
 
 from external.sample_blocks import Sampler
 from src.data.pamap2_labels import get_activity_type_from_search_space
-from src.logging.train_logger import TrainLogger
+from src.logging.supernet_logger import SupernetLogger
 from src.models.supernet import Supernet
-from src.models.train_model import train
+from src.models.train_model import train, evaluate
 from src.paths import SUPERNET_PATH
+from src.utils.yaml_io import load_pamap2_fixed_arch_config
 
 
 class SupernetTrainingWrapper(nn.Module):
     def __init__(
             self,
             supernet: Supernet,
-            search_space: dict,
             fixed_subnet_path: OrderedDict[Any, Any] | None = None,
-            fixed_architecture_yaml: dict | None = None
+            n_val_architectures: int = 5
     ):
         super().__init__()
         self.supernet = supernet
-        self.search_space = search_space
-        if fixed_subnet_path:
-            self.fixed_subnet_path = fixed_subnet_path
+        self.search_space = supernet.search_space
+
+        if fixed_subnet_path is not None:
+            self.val_architectures = [fixed_subnet_path]
         else:
-            self.fixed_subnet_path = self._init_fixed_architecture(fixed_architecture_yaml)
+            self.val_architectures = [self._get_new_sampler().construct_sample(load_pamap2_fixed_arch_config())] + [self.take_one_sample() for _ in range(n_val_architectures)]
+
+        self.active_subnet_path = self.val_architectures[0]
 
     def take_one_sample(self) -> OrderedDict[Any, Any]:
         return self._get_new_sampler().construct_sample(self.search_space)
-
-    def _init_fixed_architecture(self, fixed_architecture_config):
-        if fixed_architecture_config is not None:
-            return self._get_new_sampler().construct_sample(fixed_architecture_config)
-        return self.take_one_sample()
 
     @staticmethod
     def _get_new_sampler():
@@ -51,24 +49,37 @@ class SupernetTrainingWrapper(nn.Module):
         if self.training:
             return self.supernet(x, self.take_one_sample())
         else:
-            return self.supernet(x, self.fixed_subnet_path)
+            return self.supernet(x, self.active_subnet_path)
+
+    def evaluate_supernet(self, validation_loader, device="cuda"):
+        self.eval()
+        accuracies = []
+
+        for arch in self.val_architectures:
+            self.active_subnet_path = arch
+            summary = evaluate(
+                model=self,
+                data_loader=validation_loader,
+                device=device,
+            )
+            accuracies.append(summary.accuracy)
+
+        return accuracies
 
 
 def train_supernet(
         supernet: Supernet,
-        search_space: dict,
         epochs: int,
         device: str = "cuda",
-        fixed_architecture_config: dict = None,
         save_path: str = SUPERNET_PATH,
         do_save: bool = True,
 ):
     optuna.logging.set_verbosity(optuna.logging.WARNING)
+    search_space = supernet.search_space
 
     wrapped_supernet = SupernetTrainingWrapper(
         supernet,
-        search_space,
-        fixed_architecture_yaml=fixed_architecture_config
+        n_val_architectures=5,
     )
 
     train(
@@ -76,7 +87,7 @@ def train_supernet(
         epochs=epochs,
         device=device,
         activity_type=get_activity_type_from_search_space(search_space),
-        logger=TrainLogger(),
+        logger=SupernetLogger(),
         sequence_length=search_space["input"][1],
         load_best_weights=False,
         retraining_best_model=False,
