f59faf160c48782807fc0c5ff5b9526d75d6914e
dominik <dominik.sukic@wwb.de>
2026-09-20 15:54:05 +0200

    Make imports for analysis package

---
+++ src/analysis/__init__.py
@@ -0,0 +1,7 @@
+from .benchmark import benchmark_spos, benchmark_baseline
+from .rank_correlation import conduct_rank_correlation
+
+from .plot.evolution_scatter_plot import plot_spos_nsga_versus_random
+from .plot.learning_curve import plot_baseline_learning_curve, plot_spos_learning_curve
+from .plot.heatmap import plot_baseline_heatmap, plot_spos_heatmap
+from .plot.frequency_bar_chart import plot_spos_bar_chart, plot_baseline_bar_chart
