4b250c9e2bd144217206adf706e747da677c0908
dominik <dominik.sukic@wwb.de>
2026-09-20 15:43:53 +0200

    Added HistoryTracker to track datapoints needed to plot a learning curve

--- src/logging/summary.py
+++ src/logging/summary.py
@@ -1,4 +1,10 @@
+import os
 from dataclasses import dataclass
+from pathlib import Path
+
+import pandas as pd
+
+from src.paths import TRAIN_HISTORY_DIR
 
 
 @dataclass
@@ -6,6 +12,7 @@ class ModelSummary:
     loss: float | None
     accuracy: float | None
     f1_score: float | None
+    history: dict[str, list[float | int]] | None = None
 
     def print(self):
         print(
@@ -14,3 +21,24 @@ class ModelSummary:
             f"Accuracy: {self.accuracy:.2f} %\n"
             f"F1 Score: {self.f1_score:.2f} %\n"
         )
+
+
+class HistoryTracker:
+    def __init__(self):
+        self.history = {
+        "epoch": [],
+        "train_loss": [],
+        "val_loss": [],
+        "train_acc": [],
+        "val_acc": []
+    }
+
+    def update(self, epoch: int, train_summary: ModelSummary, val_summary: ModelSummary):
+        self.history["epoch"].append(epoch + 1)
+        self.history["train_loss"].append(train_summary.loss)
+        self.history["val_loss"].append(val_summary.loss)
+        self.history["train_acc"].append(train_summary.accuracy)
+        self.history["val_acc"].append(val_summary.accuracy)
+
+    def to_dict(self) -> dict[str, list[float | int]]:
+        return self.history
